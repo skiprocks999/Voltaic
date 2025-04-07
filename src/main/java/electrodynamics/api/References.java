3ae68ed80c887826165f6c2b6e9d89aa9c9a1c39
aurilisdev <aurilisdev@gmail.com>
2025-04-07 23:39:31 +0200

    Some switches to Voltaic (runs electro with a different name, nomodels)

--- src/main/java/electrodynamics/api/References.java
+++ src/main/java/electrodynamics/api/References.java
@@ -1,8 +1,8 @@
 package electrodynamics.api;
 
 public class References {
-	public static final String ID = "electrodynamics";
-	public static final String NAME = "Electrodynamics";
+	public static final String ID = "voltaic";
+	public static final String NAME = "Voltaic";
 
 	public static final String MEKANISM_ID = "mekanism";
 }
