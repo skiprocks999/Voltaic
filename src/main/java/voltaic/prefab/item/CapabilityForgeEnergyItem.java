e3d1e526acbf715b7d5eb555a20f47bfc65bd94f
skip999 <skiprocks999@gmail.com>
2025-04-26 09:51:45 -0500

    tweaks

--- src/main/java/voltaic/prefab/item/CapabilityForgeEnergyItem.java
+++ src/main/java/voltaic/prefab/item/CapabilityForgeEnergyItem.java
@@ -47,6 +47,6 @@ public class CapabilityForgeEnergyItem implements IEnergyStorage {
 
     @Override
     public boolean canReceive() {
-        return canExtract;
+        return canRecieve;
     }
 }
